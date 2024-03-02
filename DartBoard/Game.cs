6bf8ace45ffdfd52640fa7677bdc5fd1063a2ab8
BuildWithCloud <skybox_spells.0g@icloud.com>
2024-03-02 21:46:46 +0000

    Edited Game.cs to make it work better with different types of games

--- DartBoard/Game.cs
+++ DartBoard/Game.cs
@@ -2,39 +2,44 @@ namespace DartBoard;
 
 public abstract class Game
 {
-    private int MaxPlayers;
-    private int MinPlayers;
-    private Player[] Players;
-    private int CurrentPlayer;
-    private bool Finished;
-    
+    protected Player[] Players;
+    protected int CurrentPlayer = 0;
+    private bool _finished = false;
+    protected int DisplayWidth;
 
-    public Game()
-    {
-        Finished = false;
-    }
+
     public void DefinePlayers()
     {
         for (int i = 0; i < Players.Length; i++)
         {
             Console.Write("Please enter the name of player" + i.ToString());
-            string name = Console.ReadLine();
+            string name = Console.ReadLine() ?? "";
             Players[i] = new Player(name);
         }
     }
 
     public void Play()
     {
-        while (!Finished)
+        while (!_finished)
         {
             Display();
             EnterInfo();
-            Finished = GameFinished();
+            _finished = GameFinished();
+            if (!_finished)
+            {
+                CurrentPlayer++;
+                if (CurrentPlayer == Players.Length)
+                {
+                    CurrentPlayer = 0;
+                }
+            }
         }
     }
-    public abstract bool GameFinished();
-    public abstract void Display();
-    public abstract void EnterInfo();
-    
-    
+
+    protected abstract bool GameFinished();
+    protected abstract void Display();
+    protected abstract void EnterInfo();
+    protected abstract void DisplayWinner();
+
+
 }
