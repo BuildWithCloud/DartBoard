40fd0e10c0989c2bd9448de64554a71d7a0ce74b
BuildWithCloud <skybox_spells.0g@icloud.com>
2024-03-02 20:24:13 +0000

    Created Player Class

---
+++ DartBoard/Player.cs
@@ -0,0 +1,13 @@
+namespace DartBoard;
+
+public class Player
+{
+    private string Name;
+    public int Score;
+
+    public Player(string name)
+    {
+        Name = name;
+        Score = 0;
+    }
+}
