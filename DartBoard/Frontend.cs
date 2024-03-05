a3bf9d84e798bebe1fae889695015cdbf007b753
BuildWithCloud <skybox_spells.0g@icloud.com>
2024-03-05 19:55:08 +0000

    added inputs to Frontend.cs

--- DartBoard/Frontend.cs
+++ DartBoard/Frontend.cs
@@ -2,23 +2,35 @@ namespace DartBoard;
 
 public static class Frontend
 {
+    //outputs:
     public static void DisplayLeft (string message)
     {
         Console.WriteLine(message);
     }
-    
     public static void DisplayRight (string message)
     {
         string output = new String(' ', Console.WindowWidth - message.Length);
         output += message;
         Console.WriteLine(output);
     }
-    
     public static void DisplayLeftAndRight (string left, string right)
     {
         string output = left;
         output += new String(' ', (Console.WindowWidth - left.Length) - right.Length);
         output += right;
         Console.WriteLine(output);
+    }
+    
+    //inputs:
+    public static string? InputLeft(string message)
+    {
+        DisplayLeft(message);
+        return Console.ReadLine();
+    }
+    public static string? InputRight(string message)
+    {
+        DisplayRight(message);
+        Console.Write(new string(' ', Console.WindowWidth - message.Length));
+        return Console.ReadLine();
     }
 }
